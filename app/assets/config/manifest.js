218a1ddbb10f5ec831bef3a5f5a93fe789164d2c
Kylee Acker <kyleeacker@Kylees-MacBook-Air.local>
2017-09-01 15:24:05 -0400

    added garbage

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,12 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 dsadasdasa
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css

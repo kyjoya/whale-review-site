90ede8d43761e21b7b6ec628306d86d43268e0f6
Kylee Acker <kyleeacker@Kylees-MacBook-Air.local>
2017-09-05 10:18:31 -0400

    Reverse changes from demo

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,13 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-dsadasdasa
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
